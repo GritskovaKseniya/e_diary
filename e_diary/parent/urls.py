6d3a5a7113467d4cb038f2fdfa9474f9374cba8a
GritskovaKseniya <grickovaKS@gmail.com>
2021-05-06 23:14:17 +0300

    auth app Parent

--- e_diary/parent/urls.py
+++ e_diary/parent/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'parent'
 urlpatterns = [
-
+    path('', views.main, name='main'),
 ]
--- e_diary/parent/views.py
+++ e_diary/parent/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from core.utils import *
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+def group_required(*group_names):
+    """Requires user membership in at least one of the groups passed in."""
+
+    def in_groups(u):
+        if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
+            return True
+
+    return user_passes_test(in_groups)
+
+
+@login_required
+@group_required('parents')
+def main(request):
+    url_name = request.resolver_match.url_name
+    user = request.user
+    parent = Parents.objects.filter(user=user)[0]
+    user_name = parent.name
+    mass = "Hello, " + str(user_name) + "!"
+    return render(request, 'parent/main.html', {'text': mass})
